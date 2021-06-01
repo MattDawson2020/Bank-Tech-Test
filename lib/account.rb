66a277f9e44fc513377265602a269af43dcd16a2
Matt <mattdawson18@gmail.com>
2021-06-01 11:45:44 +0100

    User can withdraw money

--- lib/account.rb
+++ lib/account.rb
@@ -2,14 +2,16 @@ class Account
 
   attr_reader :balance
 
-  def initialize
-
-    @balance = 0
-
+  def initialize(balance = 0)
+    @balance = balance
   end
 
   def deposit(amount)
     @balance += amount
+  end
+
+  def withdraw(amount)
+    @balance -= amount
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,14 +1,22 @@
 require 'account'
 
 describe Account do
+  let(:subject) {Account.new(1000)}
   
   context '#deposit' do
 
     it 'can deposit funds' do
-      expect(subject.balance).to eq 0
-      expect(subject.deposit(500)).to eq 500
+      expect(subject.balance).to eq 1000
+      expect(subject.deposit(500)).to eq 1500
     end
-   
+  end
+
+  context '#withdraw' do
+
+    it 'can withdraw funds' do
+      expect(subject.balance).to eq 1000
+      expect(subject.withdraw(500)).to eq 500
+    end
   end
 
 end
