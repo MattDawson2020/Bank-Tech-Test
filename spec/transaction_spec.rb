de6b54903a73a0a1f1d5ac2c0d9542c6d3d70fae
Matt <mattdawson18@gmail.com>
2021-06-04 10:48:55 +0100

    Transaction spec redundancies removed

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,20 +1,13 @@
 require 'transaction'
 
 describe Transaction do
-  let(:deposit_transaction) { Transaction.new(time: Time.now, type: :deposit, amount: 500, balance: 1500) }
-  let(:withdraw_transaction) { Transaction.new(time: Time.now, type: :withdrawal, amount: 500, balance: 500) }
+  let(:transaction) { Transaction.new(time: Time.now, type: :deposit, amount: 500, balance: 1500) }
 
-  it 'can return deposit details through hash keys' do
-    expect(deposit_transaction.time.strftime('%d/%m/%Y')).to eq Time.now.strftime('%d/%m/%Y')
-    expect(deposit_transaction.amount).to eq 500
-    expect(deposit_transaction.type).to eq :deposit
-    expect(deposit_transaction.balance).to eq 1500
+  it 'can return transaction details through hash keys' do
+    expect(transaction.time.strftime('%d/%m/%Y')).to eq Time.now.strftime('%d/%m/%Y')
+    expect(transaction.amount).to eq 500
+    expect(transaction.type).to eq :deposit
+    expect(transaction.balance).to eq 1500
   end
 
-  it 'can return withdrawal details through hash keys, and be identified with type' do
-    expect(withdraw_transaction.time.strftime('%d/%m/%Y')).to eq Time.now.strftime('%d/%m/%Y')
-    expect(withdraw_transaction.amount).to eq 500
-    expect(withdraw_transaction.type).to eq :withdrawal
-    expect(withdraw_transaction.balance).to eq 500
-  end
 end
