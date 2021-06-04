770a67899d604603a8c9d6e827f22dd476dec30f
Matt <mattdawson18@gmail.com>
2021-06-04 10:46:49 +0100

    Feedback Refactoring- Removed TransactionHistory class as it is uncessary

--- lib/account.rb
+++ lib/account.rb
@@ -2,28 +2,24 @@ class Account
 
   attr_reader :balance, :transaction_history
 
-  def initialize(balance = 0, transaction_history: TransactionHistory.new)
+  def initialize(balance = 0)
     @balance = balance
-    @transaction_history = transaction_history
+    @transaction_history = []
   end
 
   def deposit(amount)
     validate_deposit(amount)
     @balance += amount
-    store_transaction(Transaction.new(time: Time.now, type: :deposit, amount: amount, balance: @balance))
+    @transaction_history << (Transaction.new(time: Time.now, type: :deposit, amount: amount, balance: @balance))
   end
 
   def withdraw(amount)
     validate_withdrawal(amount)
     @balance -= amount
-    store_transaction(Transaction.new(time: Time.now, type: :withdrawal, amount: amount, balance: @balance))
+    @transaction_history <<(Transaction.new(time: Time.now, type: :withdrawal, amount: amount, balance: @balance))
   end
 
   private
-  
-  def store_transaction(transaction)
-    @transaction_history.add_transaction(transaction)
-  end
 
   def validate_deposit(amount)
     fail 'Invalid input type, please deposit numeric currency' unless amount.is_a?(Integer) || amount.is_a?(Float)
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -15,7 +15,7 @@ describe Account do
 
     it 'creates and stores stores a deposit transaction in transaction_history' do
       subject.deposit(500)
-      transaction = subject.transaction_history.transactions.first
+      transaction = subject.transaction_history.first
 
       expect(transaction.amount).to eq 500
       expect(transaction.balance).to eq 1500
@@ -38,7 +38,7 @@ describe Account do
 
     it 'creates and stores a withdrawal transaction in transaction_history' do
       subject.withdraw(500)
-      transaction = subject.transaction_history.transactions.first
+      transaction = subject.transaction_history.first
 
       expect(transaction.amount).to eq 500
       expect(transaction.balance).to eq 500
@@ -56,9 +56,4 @@ describe Account do
     end
   end
 
-  context '#transaction_history' do
-    it 'injects a transaction history object' do
-      expect(subject.transaction_history).to be_instance_of TransactionHistory
-    end
-  end
 end
